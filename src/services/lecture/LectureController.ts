0adbaff13477e95807e4e97371480e0e616240cc
16Yongjin <yongjin0802@gmail.com>
2020-08-10 02:20:19 +0900

    update lecture count

--- src/services/lecture/LectureController.ts
+++ src/services/lecture/LectureController.ts
@@ -32,7 +32,7 @@ export const storeLectures = async (
   res: Response
 ): Promise<void> => {
   const existing = await Lecture.count();
-  if (existing >= 3772) {
+  if (existing >= 3773) {
     res.send("already stored lectures");
     return;
   }
