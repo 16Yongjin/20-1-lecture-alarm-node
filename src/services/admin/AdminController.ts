a164298df61129cf249a66767b88693b14ddb2de
16Yongjin <yongjin0802@gmail.com>
2020-08-12 14:52:06 +0900

    cron time update

--- src/services/admin/AdminController.ts
+++ src/services/admin/AdminController.ts
@@ -11,7 +11,7 @@ const readFile = promisify(readFileCb);
 
 // '*/3 * 10-16 * * *'
 export const alarmJob = new CronJob({
-  cronTime: "*/3 * 10-16 * * *",
+  cronTime: "*/5 * 10-14 * * *",
   onTick: () => {
     console.log("alarm start");
     checkLectures();
