da2566bcc493ce24451074220abb7cc0fe248ac4
16Yongjin <yongjin0802@gmail.com>
2020-09-03 21:29:03 +0900

    5 seconds

--- src/services/admin/AdminController.ts
+++ src/services/admin/AdminController.ts
@@ -11,7 +11,7 @@ const readFile = promisify(readFileCb);
 
 // '*/3 * 10-16 * * *'
 export const alarmJob = new CronJob({
-  cronTime: "*/3 * 10-15 * * *",
+  cronTime: "*/5 * 10-15 * * *",
   onTick: () => {
     console.log("alarm start");
     checkLectures();
