a34874cb1fb79ebef4bc9e8ae76a813326824ec3
16Yongjin <yongjin0802@gmail.com>
2021-03-08 15:21:04 +0900

    change cron interval

--- src/services/admin/AdminController.ts
+++ src/services/admin/AdminController.ts
@@ -11,7 +11,7 @@ const readFile = promisify(readFileCb);
 
 // '*/3 * 10-16 * * *'
 export const alarmJob = new CronJob({
-  cronTime: "*/3 * 10-15 * * *",
+  cronTime: "*/2 * 10-15 * * *",
   onTick: () => {
     console.log("alarm start");
     checkLectures();
